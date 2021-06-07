39bbc51c3b53986493f50350e69d323c09ff33d3
sniquey <veronique.eldridge@gmail.com>
2021-06-07 18:34:04 +1000

    added search function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-///////////////////////////////////////
-// code to put image thumbnails into the html div with class imgcontainer
-let html = '';
-const container = document.querySelector('.imgcontainer');
 
-for (let i=1; i<13; i++) {
-    if (i < 10) {
-    html += `<img src="photos/thumbnails/0${i}.jpg" alt="photo ${i}">`;
-    } else {
-        html += `<img src="photos/thumbnails/${i}.jpg" alt="photo ${i}">`;
-    }
-}
-
-container.innerHTML = html;
-//////////////////////////////////////////
+baguetteBox.run('.gallery');
+const search = new Filter('search', 'data-caption');
